a50a72b0f7e389beb4089fd29599b5aa6c5691aa
johancj <johancj@stud.ntnu.no>
2021-05-08 22:37:09 +0200

    commented out debugging prints

--- Projects/STM32_MPU6050_From_Scratch/src/main.cpp
+++ Projects/STM32_MPU6050_From_Scratch/src/main.cpp
@@ -42,26 +42,29 @@ void I2C_write(uint8_t device_address, uint8_t register_start_address, uint8_t d
 
 void I2C_write(uint8_t device_address, uint8_t register_start_address){
     
-    Serial1.println("0...");
+    //Serial1.println("0...");
     delay(1);
     
     //Start condition
     I2C1->CR1 |= I2C_CR1_START;
     while(!(I2C1->SR1 & I2C_SR1_SB_Msk)){} // Check for sent start condition
-    Serial1.println("1...");
+    
+    //Serial1.println("1...");
     delay(1);
 
     //Send device adress
     I2C1->DR = device_address;
     while(!(I2C1->SR1 & I2C_SR1_ADDR_Msk)){} // Wait for sent device address
     I2C1->SR2;
-    Serial1.println("2...");
+    
+    //Serial1.println("2...");
     delay(1);
 
     //send internal adress
     I2C1->DR = register_start_address;
     while(!(I2C1->SR1 & I2C_SR1_TXE)){} // Wait for byte transfer complete
-    Serial1.println("3...");
+    
+    //Serial1.println("3...");
     delay(1);
 
     //Stop condition
@@ -83,24 +86,28 @@ void I2C_read(uint8_t device_address, uint8_t register_start_address, uint8_t da
     I2C1->CR2 |= I2C_CR2_DMAEN;
     I2C1->CR2 |= I2C_CR2_LAST;
     I2C1->CR1 |= I2C_CR1_ACK;
-    Serial1.println("r0...");
+    
+    //Serial1.println("r0...");
     delay(1);
 
     //Start condition
     I2C1->CR1 |= I2C_CR1_START;
     while(!(I2C1->SR1 & I2C_SR1_SB_Msk)){} // Check for sent start condition
-    Serial1.println("r1...");
+    
+    //Serial1.println("r1...");
     delay(1);
 
     //Send device adress
     I2C1->DR = device_address + 1; // +1 because the least significant bit is the data direction bit. 1 is read.
     while(!(I2C1->SR1 & I2C_SR1_ADDR_Msk)){} // Wait for sent device address
     I2C1->SR2;
-    Serial1.println("r2...");
+    
+    //Serial1.println("r2...");
     delay(1);
     
     while((DMA1->ISR & DMA_ISR_TCIF7) == 0); // Wait for DMA tranfer conplete (This including the rest should be done using interrupt service routine)
-    Serial1.println("r4...");
+    
+    //Serial1.println("r4...");
     delay(1);
 
     DMA1_Channel7->CCR &= ~DMA_CCR_EN;  // Disable DMA channel 7
